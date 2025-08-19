5d09eaf3f8f2e6688d015506bafce21509d3f009
Meenakshi S Reddy <74555895+Meenakshi305@users.noreply.github.com>
2025-08-19 22:40:25 +0530

    a
    a

---
+++ MultiClientTest.java
@@ -0,0 +1,39 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class MultiClientTest {
+    public static void main(String[] args) throws InterruptedException {
+        int clients = 5;
+        Thread[] threads = new Thread[clients];
+
+        for (int i = 0; i <= clients; i++) {
+            final int id = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+                    Calculator calc = (Calculator) registry.lookup("CalculatorService");
+
+                    calc.pushValue(121 + id);
+                    calc.pushValue(204 + id);
+                    calc.pushValue(78 + id);
+                    calc.pushValue(21 + id);
+                    calc.pushValue(76 + id);
+                    calc.pushOperation("min");
+
+                    int result = calc.pop();
+                    if (result == Integer.MIN_VALUE) {
+                        System.out.println("The Stack is empty, no result found.");
+                    } else {
+                        System.out.println(" Result: " + result);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, "Client-" + i);
+            threads[i].start();
+        }
+
+        for (Thread t : threads) t.join();
+        System.out.println("Multi-client test completed.");
+    }
+}
